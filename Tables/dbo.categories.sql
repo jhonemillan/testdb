7ae7785a07782d8f2fab9bdadd0f6e89e8a3ceeb
unknown <jemillan@PARMAC>
2018-11-01 10:46:52 -0500

    se grega la columna por que lo pidieron

--- Tables/dbo.categories.sql
+++ Tables/dbo.categories.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[categories]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
 [Name] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[DateCreated_dtm] [datetime] NOT NULL CONSTRAINT [DF_Categories_DateCreated_dtm] DEFAULT (getdate())
+[DateCreated_dtm] [datetime] NOT NULL CONSTRAINT [DF_Categories_DateCreated_dtm] DEFAULT (getdate()),
+[code] [nvarchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[categories] ADD CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
